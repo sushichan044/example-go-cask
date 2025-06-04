131c993473a40886128adb06692d91651c795169
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-04 23:12:37 +0900

    aaa

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sushichan044/example-go-cask/version"
+)
 
 func main() {
+	fmt.Println(version.Version)
 	fmt.Println("Hello, World!")
 }
